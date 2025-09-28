ceb3fbbe108fcfaff1091397708cf6fcdcb911e2
Shahid Khan <shahidjoura307@gmail.com>
2025-09-28 20:06:22 +0530

    add localstorage in todo

--- To Do APP/script.js
+++ To Do APP/script.js
@@ -2,16 +2,31 @@ const add = document.querySelector('#addbtn')
 const input = document.querySelector('#input')
 const todolist = document.querySelector('.todo-container')
 
+let editTodo = null
+
 function updateEmpty() {
     const emptyMsg =  document.querySelector('#empty')
     emptyMsg.style.display = todolist.children.length === 0 ? "block" : "none";
 }
 
+function saveTodos(){
+    const todos = []
+    todolist.querySelectorAll('.todo span').forEach(todo=>{
+        todos.push(todo.innerText)
+    })
 
-add.addEventListener('click', () => {
-    const value = input.value.trim();
-    if (!value) return;
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
 
+function loadTodos(){
+    const todos = JSON.parse(localStorage.getItem('todos')) || [];
+    todos.forEach(todoText => {
+        createTodo(todoText);
+    });
+    updateEmpty();
+}
+
+function createTodo(value){
     const todo = document.createElement('div')
     todo.classList.add("todo");
 
@@ -32,18 +47,44 @@ add.addEventListener('click', () => {
     
     del.addEventListener('click', () => {
         todolist.removeChild(todo);
+        saveTodos();
         updateEmpty();
+        if(editTodo==span){
+            input.value = ''
+            editTodo = null
+            add.textContent = 'Add'
+            add.style.backgroundColor = '#1d9929';
+        }
     });
+
+    edit.addEventListener('click', ()=>{
+        input.value = span.innerText;
+        editTodo = span;
+        add.textContent = 'edit'
+        add.style.backgroundColor = '#d1a909ff';
+    })
         
     btns.append(edit, del)
     todo.append(span, btns)
-
-    input.value = '';
     todolist.prepend(todo)
+}
+
+add.addEventListener('click', () => {
+    const value = input.value.trim();
+    if (!value) return;
+
+    if(editTodo){
+        editTodo.innerText = value;
+        editTodo = null;
+        add.textContent = 'Add';
+        add.style.backgroundColor = '';
+    }
+    else{
+        createTodo(value)
+    }
+    saveTodos()
+    input.value = '';
     updateEmpty();
 });
 
-updateEmpty();
-
-
-
+loadTodos();
